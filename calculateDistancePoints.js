40022f77ea751d78fd020f6e7ff6e3371f8d0296
Tomasz Sokalski <tomm1slaw@o2.pl>
2018-11-20 23:26:20 +0100

    Add mentor suggest

--- calculateDistancePoints.js
+++ calculateDistancePoints.js
@@ -1,26 +1,18 @@
 const calculateDistancePoints = (distance, hillSize, kPoint) => {
     if (typeof hillSize !== 'string' || typeof distance !== 'number' || typeof kPoint !== 'number') {
-        
         return 'wrong type of data';
-        
     } else {
-
         switch (hillSize) {
             case 'normal':
                 return 60 + ((distance - kPoint) * 2);
-              
             case 'big':
-                return 60 + ((distance - kPoint) * 1.8); 
-            
+                return 60 + ((distance - kPoint) * 1.8);
             case 'mamut':
                 return 120 + ((distance - kPoint) * 1.2);
-                                        
             default:
                 return 'wrong hillSize';
         }
-
     }
-
 };
-  
+
 module.exports = calculateDistancePoints;
--- calculateStylePoints.js
+++ calculateStylePoints.js
@@ -1,38 +1,22 @@
 const calculateStylePoints = (styleNotes) => {
-    
     if (!Array.isArray(styleNotes)) {
-
-    return 'wrong type od styleNotes';
-
+        return 'wrong type od styleNotes';
     } else if (styleNotes.length !== 5) {
-    
-    return "there are not enough judges'notes";
-
-    } else if (styleNotes.filter( note => typeof note === 'number').length < 5) {
-    
-    return "judges'notes should be a number";
-    
+        return "there are not enough judges'notes";
+    } else if (styleNotes.filter(note => typeof note === 'number').length < 5) {
+        return "judges'notes should be a number";
     }
-
     const maxValue = Math.max.apply(Math, styleNotes);
-
     const indexOfMaxValue = styleNotes.indexOf(maxValue);
-  
     styleNotes.splice(indexOfMaxValue, 1);
-
     const minValue = Math.min.apply(Math, styleNotes);
-  
     const indexOfMinValue = styleNotes.indexOf(minValue);
-  
     styleNotes.splice(indexOfMinValue, 1);
-
-    let suma = 0;   
-    for (let i = 0; i < styleNotes.length; i++) { 
-        suma = suma + styleNotes[i]; 
-    } 
-
-    return suma;
-    
+    let amount = 0;
+    for (let i = 0; i < styleNotes.length; i++) {
+        amount = amount + styleNotes[i];
+    }
+    return amount;
 };
-  
+
 module.exports = calculateStylePoints;
